ba8d33a7a5d48980341bab01712e7e8ef214fb72
Pedro Fonseca <fonseca.pmvasco@gmail.com>
2020-10-11 22:45:37 +0100

    Created server and added db invocations

---
+++ src/getDB.ts
@@ -0,0 +1,23 @@
+import { JsonDB } from 'node-json-db';
+import { Config } from 'node-json-db/dist/lib/JsonDBConfig';
+
+const loadedDBs = {} as { [key: string]: JsonDB };
+
+// Initializes a new dababase. If there's already a file with that name, it loads it instead.
+// The first argument is the database filename. If no extension, '.json' is assumed and automatically added.
+// The second argument is used to tell the DB to save after each push
+// If you put false, you'll have to call the save() method.
+// The third argument is to ask JsonDB to save the database in an human readable format. (default false)
+// The last argument is the separator. By default it's slash (/)
+const initDB = (name: string): JsonDB =>
+  new JsonDB(new Config(name, true, false, '/'));
+
+// Fetches a loaded database or initializes it if it's not loaded yet
+export default (name: string): JsonDB => {
+  // If the db is not loaded yet then we create it
+  if (loadedDBs[name] === undefined) {
+    loadedDBs[name] = initDB(name);
+  }
+
+  return loadedDBs[name];
+};
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,87 @@
 import express from 'express';
+import { config } from 'dotenv';
+import { JsonDB } from 'node-json-db';
+import getDB from './getDB';
 
+config();
+const port = process.env.PORT || 3000;
 const app = express();
+app.use(express.json());
 
 app.get('/', (request, response) =>
   response.json({
-    message: 'My express server, Typescript e ESLint!',
+    message:
+      'DB server up and running! Use format /dbname to access a database',
   }),
 );
 
-app.listen(3333, () => {
-  console.log('Back-end started in 3333 port!');
+type ObjectType = { [key: string]: unknown };
+
+const logMessage = (message: string): void => {
+  if (process.env.DEBUG === 'TRUE') console.log(`${message}`);
+};
+
+const executeOperation = (
+  dbname: string,
+  operation: (db: JsonDB) => ObjectType | void,
+): ObjectType => {
+  try {
+    const db = getDB(dbname);
+    const result = operation(db);
+
+    logMessage(`Success ${operation}`);
+    return {
+      success: true,
+      data: result,
+    };
+  } catch (error) {
+    logMessage(`Error ${operation}: ${error.message}`);
+    return {
+      success: false,
+      operation,
+      error: error.message,
+    };
+  }
+};
+
+app.get('/:dbname', (request, response) => {
+  const path = request.query.path as string;
+  const operation = (db: JsonDB) => db.getData(path);
+  const result = executeOperation(request.params.dbname, operation);
+
+  response.json(result);
+});
+
+app.post('/:dbname', (request, response) => {
+  const path = request.query.path as string;
+  const operation = (db: JsonDB) => db.push(path, request.body, true);
+  const result = executeOperation(request.params.dbname, operation);
+
+  response.json(result);
+});
+
+app.put('/:dbname', (request, response) => {
+  const path = request.query.path as string;
+  const operation = (db: JsonDB) => db.push(path, request.body, true);
+  const result = executeOperation(request.params.dbname, operation);
+
+  response.json(result);
+});
+
+app.patch('/:dbname', (request, response) => {
+  const path = request.query.path as string;
+  const operation = (db: JsonDB) => db.push(path, request.body, false);
+  const result = executeOperation(request.params.dbname, operation);
+
+  response.json(result);
+});
+
+app.get('/:dbname/reload', (request, response) => {
+  const db = getDB(request.params.dbname);
+  db.reload();
+  response.json({ success: true });
+});
+
+app.listen(port, () => {
+  console.log(`Back-end started in ${port} port!`);
 });
